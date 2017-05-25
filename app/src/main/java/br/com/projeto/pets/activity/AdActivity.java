4ac8d1ef405da2a084a4e29e5637ef469d2729e6
Samir Rolemberg <srolemberg@outlook.com>
2017-05-25 10:35:48 -0300

    Spinner Inits Mocks

--- app/src/main/java/br/com/projeto/pets/activity/AdActivity.java
+++ app/src/main/java/br/com/projeto/pets/activity/AdActivity.java
@@ -3,8 +3,13 @@ package br.com.projeto.pets.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
+import android.widget.ArrayAdapter;
 
 import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.projeto.pets.R;
 import br.com.projeto.pets.utils.ActivityImpl;
@@ -14,6 +19,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class AdActivity extends AppCompatActivity implements ActivityImpl {
 
     private CircleImageView imgUser01, imgUser02, imgUser03;
+    private AppCompatSpinner spnBreed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,8 @@ public class AdActivity extends AppCompatActivity implements ActivityImpl {
         imgUser01 = (CircleImageView) findViewById(R.id.imgUser01);
         imgUser02 = (CircleImageView) findViewById(R.id.imgUser02);
         imgUser03 = (CircleImageView) findViewById(R.id.imgUser03);
+        spnBreed = (AppCompatSpinner) getActivity().findViewById(R.id.spnBreed);
+
     }
 
     @Override
@@ -51,10 +59,27 @@ public class AdActivity extends AppCompatActivity implements ActivityImpl {
     public void init() {
         //Util.colorStatusBar(this, getResources().getColor(R.color.accent3));
         Util.colorNavigationBar(this, getResources().getColor(R.color.primary));
+
+        initSpn();
     }
 
     @Override
     public Activity getActivity() {
         return this;
+    }
+
+    private void initSpn() {
+        List<String> labels = new ArrayList<>();
+        labels.add("Raça");
+
+        String[] arrayMock = getResources().getStringArray(R.array.mock_array);
+
+
+        for (int i = 0; i < arrayMock.length; i++) {
+            labels.add(arrayMock[i]);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, labels);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnBreed.setAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/br/com/projeto/pets/fragment/AdoptionFilterFragment.java
+++ app/src/main/java/br/com/projeto/pets/fragment/AdoptionFilterFragment.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.projeto.pets.R;
 import br.com.projeto.pets.utils.ActivityImpl;
@@ -15,6 +20,8 @@ public class AdoptionFilterFragment extends Fragment implements ActivityImpl {
 
     private static final String ARG_TAG = "ARG_TAG";
     private String mFragmentTag;
+
+    private AppCompatSpinner spnBreed;
 
     private OnFragmentInteractionListener mListener;
 
@@ -71,7 +78,7 @@ public class AdoptionFilterFragment extends Fragment implements ActivityImpl {
 
     @Override
     public void bind() {
-
+        spnBreed = (AppCompatSpinner) getActivity().findViewById(R.id.spnBreedAdoption);
     }
 
     @Override
@@ -81,9 +88,24 @@ public class AdoptionFilterFragment extends Fragment implements ActivityImpl {
 
     @Override
     public void init() {
-
+        initSpn();
     }
 
     public interface OnFragmentInteractionListener {
+    }
+
+    private void initSpn() {
+        List<String> labels = new ArrayList<>();
+        labels.add("Raça");
+
+        String[] arrayMock = getResources().getStringArray(R.array.mock_array);
+
+
+        for (int i = 0; i < arrayMock.length; i++) {
+            labels.add(arrayMock[i]);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, labels);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnBreed.setAdapter(arrayAdapter);
     }
 }
