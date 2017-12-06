04d3de9b228c5bebea7fd2500d66cb62bab105aa
Felipe Theodoro Goncalves <ftgoncalves@gmail.com>
2017-12-05 22:13:32 -0200

    Init

---
+++ app/src/main/java/br/com/projeto/pets/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.projeto.pets
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
