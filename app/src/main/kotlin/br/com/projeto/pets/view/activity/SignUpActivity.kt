6ef09f3780b971433ed907c9f695001337c43c24
Rodrigo Henriques <rsh.trabalho@gmail.com>
2017-12-12 23:48:30 -0200

    Create signup activity

---
+++ app/src/main/kotlin/br/com/projeto/pets/view/activity/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package br.com.projeto.pets.view.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.projeto.pets.R
+
+class SignUpActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sign_up)
+  }
+}
