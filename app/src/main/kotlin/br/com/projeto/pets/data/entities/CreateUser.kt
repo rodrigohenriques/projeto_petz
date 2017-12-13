7346bcb1cf7da6f33cfd397d4ee5ca96c0b714a2
Rodrigo Henriques <rsh.trabalho@gmail.com>
2017-12-13 00:49:54 -0200

    Add reference to create user entity

--- app/src/main/kotlin/br/com/projeto/pets/data/entities/CreateUser.kt
+++ app/src/main/kotlin/br/com/projeto/pets/data/entities/CreateUser.kt
@@ -1,5 +1,6 @@
 package br.com.projeto.pets.data.entities
 
+// Ref: https://github.com/ProjetoPets/api/blob/master/docs/api/user/create.json
 data class CreateUser(
     val name: String,
     val email: String,
