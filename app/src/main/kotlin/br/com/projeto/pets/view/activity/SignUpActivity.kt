b9025762abd623261d30c357af326e1e7abc3fa1
Rodrigo Henriques <rodrigo.henriques@99taxis.com>
2018-01-04 22:57:57 -0200

    Improve login activity layout and signup activity layout

--- app/src/main/kotlin/br/com/projeto/pets/view/activity/SignUpActivity.kt
+++ app/src/main/kotlin/br/com/projeto/pets/view/activity/SignUpActivity.kt
@@ -9,5 +9,7 @@ class SignUpActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_sign_up)
+
+
   }
 }
