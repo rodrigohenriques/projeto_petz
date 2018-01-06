2f2f62e9d6ad1604efed37b706a005edeb1edc98
Rodrigo Henriques <rodrigo.henriques@99taxis.com>
2018-01-05 23:29:13 -0200

    Fix dependencies and a crash

--- app/src/main/kotlin/br/com/projeto/pets/SignUpStateManager.kt
+++ app/src/main/kotlin/br/com/projeto/pets/SignUpStateManager.kt
@@ -2,8 +2,9 @@ package br.com.projeto.pets
 
 import br.com.projeto.pets.contracts.SignUpContract
 import br.com.projeto.pets.states.SignUpState
+import javax.inject.Inject
 
-class SignUpStateManager : StateManager<SignUpState>(SignUpState()) {
+class SignUpStateManager @Inject constructor() : StateManager<SignUpState>(SignUpState()) {
   fun updateData(form: SignUpContract.Form) {
     val errors = form.validate()
     val newState = state().copy(data = form, errors = errors)
