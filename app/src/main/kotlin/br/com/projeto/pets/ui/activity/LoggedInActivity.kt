b23e5c22ed041aaaadc4d413ee7bbde273546db2
Bruno Colombini <brunocolombini@Brunos-MacBook-Pro-2.local>
2018-04-02 00:42:45 -0300

    start list

--- app/src/main/kotlin/br/com/projeto/pets/ui/activity/LoggedInActivity.kt
+++ app/src/main/kotlin/br/com/projeto/pets/ui/activity/LoggedInActivity.kt
@@ -4,20 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.TabLayout
 import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import br.com.projeto.pets.R
 import br.com.projeto.pets.adapter.CustomPagerAdapter
-import br.com.projeto.pets.fragment.SellingFragment
+import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_logged_in.*
 import kotlinx.android.synthetic.main.base_view.*
 
-class LoggedInActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class LoggedInActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         return true
     }
@@ -31,15 +28,14 @@ class LoggedInActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
         return true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in)
-
+        setSupportActionBar(toolbar)
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
@@ -51,6 +47,24 @@ class LoggedInActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
         pagerTitle.getTabAt(0)!!.text = "Venda"
         pagerTitle.getTabAt(1)!!.text = "Adoção"
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            (R.id.ads_filter) -> {
+
+            }
+            (R.id.profile) -> {
+
+            }
+            (R.id.ad) -> {
+
+            }
+            else -> {
+                return false
+            }
+        }
+        return true
     }
 
     companion object {
