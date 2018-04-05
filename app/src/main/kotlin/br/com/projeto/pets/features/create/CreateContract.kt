7878c831e3d7f30fa1e494eba4456527b4c49a8e
dev <dev@imac-de-m4u-2.m4ucorp.dmc>
2018-04-05 13:49:37 -0300

    create struct for another views

--- app/src/main/kotlin/br/com/projeto/pets/features/create/CreateContract.kt
+++ app/src/main/kotlin/br/com/projeto/pets/features/create/CreateContract.kt
@@ -1,5 +1,8 @@
 package br.com.projeto.pets.features.pet
 
-/**
- * Created by dev on 05/04/2018.
- */
+
+interface CreateContract {
+    interface View
+
+    interface Presenter
+}
---
+++ app/src/main/kotlin/br/com/projeto/pets/features/filter/fragment/FilterFragmentPresenter.kt
@@ -0,0 +1,7 @@
+package br.com.projeto.pets.features.filter.fragment
+
+interface FilterFragmentPresenter {
+    interface View
+
+    interface Presenter
+}
