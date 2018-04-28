00321563c61703c10891ce47c3569db01e9077d1
Rodrigo Henriques <rsh.trabalho@gmail.com>
2018-04-28 13:51:23 -0300

    Create module pet

--- app/src/main/kotlin/br/com/projeto/pets/features/ad/AdRepository.kt
+++ app/src/main/kotlin/br/com/projeto/pets/features/ad/AdRepository.kt
@@ -7,5 +7,3 @@ import javax.inject.Inject
 class AdRepository @Inject constructor(private val adApi: AdApi) {
   fun getAds(queryParams: QueryParams?): Single<List<Ad>> = adApi.getAllAd(queryParams?.breedId,queryParams?.ageClassificationId,queryParams?.locale)
 }
-
-data class Result<out T>(val result: T)
