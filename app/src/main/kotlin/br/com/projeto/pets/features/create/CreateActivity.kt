440e5c99ef74e333c0968226e5dad9a9f0f19327
Bruno Colombini <bruno.colombini@me.com>
2018-05-15 00:57:13 -0300

    another card

--- app/src/main/kotlin/br/com/projeto/pets/features/create/CreateActivity.kt
+++ app/src/main/kotlin/br/com/projeto/pets/features/create/CreateActivity.kt
@@ -5,12 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import br.com.projeto.pets.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_create.*
 
-class CreateActivity : DaggerAppCompatActivity(), CreateContract.View{
+
+class CreateActivity : DaggerAppCompatActivity(), CreateContract.View {
+
+    val PICK_IMAGE: Int = 707
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        add_photo.setOnClickListener {
+        }
     }
 
     override fun onDestroy() {
