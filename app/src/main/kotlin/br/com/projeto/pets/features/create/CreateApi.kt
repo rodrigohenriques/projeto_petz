d8ebedd1b77bb5fb1cabbfbdf0d6055be79cdf0d
Bruno Colombini <bruno.colombini@me.com>
2018-05-28 23:26:14 -0300

    Card information

--- app/src/main/kotlin/br/com/projeto/pets/features/create/CreateApi.kt
+++ app/src/main/kotlin/br/com/projeto/pets/features/create/CreateApi.kt
@@ -2,11 +2,12 @@ package br.com.projeto.pets.features.create
 
 import io.reactivex.Single
 import okhttp3.Response
+import okhttp3.ResponseBody
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 
 interface CreateApi {
     @POST("/api/advertisement/")
-    fun createAd(@Body adCreate:AdCreateModel) : Single<Response>
+    fun createAd(@Body adCreate:AdCreateModel) : Single<ResponseBody>
 }
